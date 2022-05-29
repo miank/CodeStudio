3831527f7670fe3220107b81b55d8afb9f830b08
Ankit Mishra <ankit1703@gmail.com>
2022-05-29 16:24:27 +0530

    Add project files.

---
+++ FirstAndLastPositionOfAnElementInSortedArray/FirstAndLastPositionOfAnElementInSortedArray.cpp
@@ -0,0 +1,26 @@
+// FirstAndLastPositionOfAnElementInSortedArray.cpp 
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+	// Write your code here
+}
+
+int main()
+{
+	int n(0), k(0), ele(0);
+	cin >> n >> k;
+	vector<int> v;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> ele;
+		v.push_back(ele);
+	}
+
+
+}
